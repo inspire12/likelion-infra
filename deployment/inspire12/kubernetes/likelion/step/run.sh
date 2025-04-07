b41ae68b5889411b4f0788301cc13d43eeefa2b0
inspire12 <ox4443@naver.com>
2025-04-07 22:23:06 +0900

    feat(k8s-step-practice): practice step2

--- deployment/inspire12/kubernetes/likelion/step/run.sh
+++ deployment/inspire12/kubernetes/likelion/step/run.sh
@@ -3,10 +3,14 @@ kubectl create ns likelion
 kubectl config set-context --current --namespace=likelion
 kubectl config view --minify --output 'jsonpath={..namespace}'; echo
 
-# pod 실행
+# pod 실행, imagePullError 해결
 kubectl run likelion-pod --image=likelion-backend:latest --port=8082
 
 kubectl get pods
 kubectl describe pod likelion-pod
 kubectl delete pod likelion-pod
 
+# service 실행, 접근 확인
+kubectl apply -f service.yaml
+kubectl get svc
+kubectl port-forward svc/backend-service 8082:8082
